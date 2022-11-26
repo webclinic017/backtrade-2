d2a1e8a1a3aae76310c4a221666345328a374247
34j <55338215+34j@users.noreply.github.com>
2022-11-26 23:56:25 +0900

    fix: fix version

--- src/backtrade/__init__.py
+++ src/backtrade/__init__.py
@@ -1,4 +1,4 @@
-__version__ = "0.0.1"
+__version__ = "0.0.0"
 from .backtest import Backtester, BacktestResult, CloseData
 from .order import LimitOrder, MarketOrder, OrderBase, _IndexType, _OrderType
 
