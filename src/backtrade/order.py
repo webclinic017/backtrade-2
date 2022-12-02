7db5e204a43073e86427e1a5d7589d2676ff5087
34j <55338215+34j@users.noreply.github.com>
2022-12-02 21:09:31 +0900

    fix: fix OrderBase typing (#22)

--- src/backtrade/order.py
+++ src/backtrade/order.py
@@ -1,34 +1,36 @@
 from abc import ABCMeta
-from typing import Hashable, TypeVar, Union
+from typing import Any, Generic, Hashable, TypeVar, Union
 
 import attrs
 
 from .validator import _na_validator
 
+_TOrder = TypeVar("_TOrder", bound="OrderBase[Any]")
+
 
 @attrs.frozen(kw_only=True)
-class OrderBase(metaclass=ABCMeta):
+class OrderBase(Generic[_TOrder], metaclass=ABCMeta):
     size: float = attrs.field(validator=_na_validator)
 
-    def __mul__(self, other: float) -> "OrderBase":
+    def __mul__(self: "_TOrder", other: float) -> "_TOrder":
         if not isinstance(other, float):
             return NotImplemented  # type: ignore
         if other < 0:
             raise ValueError(f"Cannot multiply by negative number: {other}")
         return attrs.evolve(self, size=self.size * other)
 
-    def __div__(self, other: float) -> "OrderBase":
+    def __div__(self: "_TOrder", other: float) -> "_TOrder":
         return self * (1 / other)
 
 
 @attrs.frozen(kw_only=True)
-class LimitOrder(OrderBase):
+class LimitOrder(OrderBase["LimitOrder"]):
     price: float = attrs.field(validator=_na_validator)
     post_only: bool = attrs.field(validator=_na_validator)
 
 
 @attrs.frozen(kw_only=True)
-class MarketOrder(OrderBase):
+class MarketOrder(OrderBase["MarketOrder"]):
     pass
 
 
